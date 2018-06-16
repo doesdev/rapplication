fa3c8fd08dafcead0c7b84d25af6e5732b1a12d3
Andrew Carpenter <andrew@audioinhd.com>
2018-06-16 19:59:43 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+<div align="center">
+  <img src="assets/logo.png" alt="Subliminal Spectrum" width="400" />
+</div>
+
+# rapplication [![NPM version](https://badge.fury.io/js/rapplication.svg)](https://npmjs.org/package/rapplication)   [![js-standard-style](https://img.shields.io/badge/code%20style-standard-brightgreen.svg?style=flat)](https://github.com/feross/standard)   
+
+> Rapping a js application over Wu-Tang Clan beat (Tearz)
+
+## Why?
+
+I just wanted to see if I could rap source code for a working application.
+
+## Project Goals
+
+1. Try not to be too cheesy, cause you know, it's a novelty item
+2. It should be a functional application
+  - Every lyric should be able to go in the code without breaking functionality
+  - It should be verbose enough to work, but abstract enough to flow
+
+## Goals Achieved?
+
+1. Eh, maybe
+2. Yes, with caveats
+  - The end user must be knowledgeable in JS terminology and programming
+  - Blocks, lines, parenthesis, etc... are all implicit
+  - Requires node env and installing axios (`npm i axios`)
+  - Variable names are spoken as multiple words, need concatenated
+
+# license
+
+MIT © [Andrew Carpenter](https://github.com/doesdev)
---
+++ index.js
@@ -0,0 +1,50 @@
+'use strict'
+
+/*
+  1) const object destructure, get, equals require axios
+*/
+const { get } = require('axios')
+
+/*
+  2) let this joint is lit, equals then open backtick
+  1) h double t p s colon and slash slashin
+  2) subliminal spectrum dot com slash app dash rapping
+  1) close the string down with a backtick
+*/
+let thisJointIsLit = `https://subliminalspectrum.com/app-rapping`
+
+/*
+  2) slash slash, here comes the magic
+  1) set the function up to grab it
+  2) put it up so you can have it
+*/
+// here comes the magic, set the function up to grab it, put it up so you can have it
+
+/*
+  1) function, we got that fire, destructure data from parameter
+*/
+function weGotThatFire ({data}) {
+  /*
+    2) require fs, write file, subspec is gonna damage ya
+    1) data is arg two and then of course we've got the callback
+  */
+  require('fs').writeFile('subspecIsGonnaDamageYa', data, (error) => {
+    /*
+      2) param error, if error, throw error, comment no fallback
+    */
+    if (error) throw error // no fallback
+  })
+}
+
+/*
+  1) get, this joint is lit, arg two it is an object
+  2) with the property responseType, value's arraybuffer
+  1) dot then, we got that fire, dual slash we're done
+*/
+get(thisJointIsLit, {responseType: 'arraybuffer'}).then(weGotThatFire)
+/*
+  2) here's a sampling of my music, open zip and have some fun
+*/
+// we're done, here's a sampling of my music, open zip and have some fun
+
+module.exports = thisJointIsLit
